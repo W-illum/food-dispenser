2e4c869c8695b72cba965d2cd331f95b004337e4
William <williamillum@gmail.com>
2026-03-06 00:42:03 +0100

    added rtc and tested lidar code

--- lib/Feeder/feeder.cpp
+++ lib/Feeder/feeder.cpp
@@ -4,21 +4,49 @@ Feeder::Feeder()
     : lcd(),
       enc( ENCODER_SW, ENCODER_A, ENCODER_B ),
       stepper( TMC_DIR, TMC_STEP, TMC_MS2, TMC_MS1, TMC_EN ),
-      menu( lcd, schedule ) {}
+      menu( lcd, schedule ),
+      rtc( DS_RST, DS_CLK, DS_DAT ) {}
 
 void Feeder::begin()
 {
-    Wire.begin(SDA, SCL);
+    Wire.begin( LCD_SDA, LCD_SCL );
     lcd.begin();
     enc.begin();
     stepper.begin();
 
     // TODO: Load eeprom to fill schedule and food amount
-    menu.begin( food_amount );
+    rtc.init();
+    menu.begin( food_amount, system_clock );
 }
 
 void Feeder::update()
 {
+    rtc.getDateTime(&dt);
+    system_clock.hour = dt.hour;
+    system_clock.min = dt.minute;
+
+    static uint8_t last_min = 0;
+    if( dt.minute != last_min )
+    {
+        last_min = dt.minute;
+        Serial.printf("%02d:%02d", dt.hour, dt.minute);
+    }
+
+    /*
+    int16_t t = pulseIn(LIDAR_PWM, HIGH);
+    if ( t == 0 )
+        Serial.println("timeout");
+    else if ( t > 1850 )
+        Serial.println("no detection");
+    else
+    {
+        int16_t d = (t - 1000 ) * 3 / 4;
+        if ( d < 0 ) d = 0;
+        Serial.print(d);
+        Serial.println(" mm");
+    }
+    delay(500);*/
+
     EncoderEvent enc_e = enc.checkUpdate();
     if ( enc_e != EncoderEvent::NONE)
     {
@@ -30,10 +58,12 @@ void Feeder::update()
 
             case EncoderEvent::CW:
                 menu.onRotate( 1 );
+                stepper.rotate(45, Dir::CW);
                 break;
 
             case EncoderEvent::CCW:
                 menu.onRotate( -1 );
+                stepper.rotate(45, Dir::CCW);
                 break;
         }
     }
@@ -56,6 +86,14 @@ void Feeder::update()
             case MenuEvent::Type::FOOD_UPDATED:
                 food_amount = menu_e.updated_grams;
                 // TODO: Save to eeprom
+                break;
+
+            case MenuEvent::Type::CLOCK_UPDATED:
+                system_clock = menu_e.updated_system_clock;
+                dt.hour = system_clock.hour;
+                dt.minute = system_clock.min;
+                dt.second = 0;
+                rtc.setDateTime(&dt);
                 break;
         }
     }
--- lib/Feeder/feeder.h
+++ lib/Feeder/feeder.h
@@ -5,18 +5,23 @@
 #include "tmc_stepper.h"
 #include "menu.h"
 #include "feeding_schedule.h"
+#include "Ds1302.h"
 
-#define LIDAR_PWM   GPIO_NUM_6  // D3
-#define TMC_DIR     GPIO_NUM_7  // D4
-#define TMC_STEP    GPIO_NUM_8  // D5
-#define TMC_MS2     GPIO_NUM_9  // D6
-#define TMC_MS1     GPIO_NUM_10 // D7
-#define TMC_EN      GPIO_NUM_17 // D8
-#define ENCODER_SW  GPIO_NUM_18 // D9
-#define ENCODER_A   GPIO_NUM_21 // D10
-#define ENCODER_B   GPIO_NUM_38 // D11
-#define SDA         GPIO_NUM_11 // A4
-#define SCL         GPIO_NUM_12 // A5
+#define DS_RST      GPIO_NUM_47 // D12
+#define DS_DAT      GPIO_NUM_38 // D11
+#define DS_CLK      GPIO_NUM_21 // D10
+#define ENCODER_SW  GPIO_NUM_9  // D6
+#define ENCODER_B   GPIO_NUM_8  // D5
+#define ENCODER_A   GPIO_NUM_7  // D4
+#define LIDAR_PWM   GPIO_NUM_5  // D2
+#define LCD_SDA     GPIO_NUM_44 // D1
+#define LCD_SCL     GPIO_NUM_43 // D0
+
+#define TMC_DIR     GPIO_NUM_3  // A2
+#define TMC_STEP    GPIO_NUM_4  // A3
+#define TMC_MS2     GPIO_NUM_11 // A4
+#define TMC_MS1     GPIO_NUM_12 // A5
+#define TMC_EN      GPIO_NUM_13 // A6
 
 class Feeder
 {
@@ -31,6 +36,9 @@ private:
     TMCStepper stepper;
     Menu menu;
     FeedingSchedule schedule;
+    Ds1302 rtc;
 
     uint8_t food_amount = 0;
+    Ds1302::DateTime dt;
+    Time_t system_clock;
 };
